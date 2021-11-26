3c40e809879fd0f763d91bca829d42f4ee21d6c7
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-11-26 17:21:58 +0100

    create action and reducer components/files

---
+++ src/actions/Actions.js
@@ -0,0 +1,13 @@
+// Action types
+
+export const SET_MOVIES = "SET_MOVIES";
+export const SET_FILTER = "SET_FILTER";
+
+// Action creators
+
+export function setMovies(value) {
+  return { type: SET_MOVIES, value };
+}
+export function setFilter(value) {
+  return { type: SET_FILTER, value };
+}
