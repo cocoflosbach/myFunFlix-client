25bfe5febc758d6833a5db8f3f13f69eb3aa7758
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-12-14 19:02:32 +0100

    Replace Browser router by  Hash router

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 
 // Redux step 1: import the SET_MOVIES action
 import { setMovies } from "../../actions/Actions";
