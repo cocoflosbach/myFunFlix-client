7e1a6ee353efc05884ac73625375c5fc3c918ed2
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-12-14 19:23:21 +0100

    Update navBar Link

--- src/Components/navigation-bar/navigation-bar.jsx
+++ src/Components/navigation-bar/navigation-bar.jsx
@@ -19,7 +19,7 @@ export class NavigationBar extends React.Component {
   render() {
     const username = localStorage.getItem("user");
     const movies = `/`;
-    const profile = `#/users/${username}`;
+    const profile = `/users/${username}`;
     const logout = `/`;
 
     return (
@@ -65,39 +65,34 @@ export class NavigationBar extends React.Component {
                       <Menu.Items className="origin-top-right relative right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              as={Link}
-                              key={profile}
-                              href={profile}
+                            <Link
+                              to={profile}
                               className={
                                 (active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700 hover:text-gray-400 ")
                               }
                             >
                               Your Profile
-                            </a>
+                            </Link>
                           )}
                         </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              as={Link}
-                              key={movies}
-                              href={`/`}
+                            <Link
+                              to={`/`}
                               className={
                                 (active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700 hover:text-gray-400")
                               }
                             >
                               Movies
-                            </a>
+                            </Link>
                           )}
                         </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              href={"/"}
-                              key={logout}
+                            <Link
+                              to={"/"}
                               onClick={this.onLoggedOut}
                               className={
                                 (active ? "bg-gray-100" : "",
@@ -105,7 +100,7 @@ export class NavigationBar extends React.Component {
                               }
                             >
                               Sign out
-                            </a>
+                            </Link>
                           )}
                         </Menu.Item>
                       </Menu.Items>
